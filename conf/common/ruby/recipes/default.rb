bfaaeed4f56a42c6b6e7c1c87cfbfba34ceea56d
Adrian Nöthlich <git@promasu.tech>
2020-05-12 10:33:32 +0200

    Fix order of package installation
    Signed-off-by: Adrian Nöthlich <git@promasu.tech>

--- conf/common/ruby/recipes/default.rb
+++ conf/common/ruby/recipes/default.rb
@@ -1,5 +1,5 @@
 %w{
-  ruby2.0 build-essential libssl-dev libssl1.0.0=1.0.1-3ubuntu1.6 zlib1g-dev libssl-doc libsqlite3-dev libyaml-dev
+  ruby2.0 build-essential libssl1.0.0=1.0.1-3ubuntu1.6 zlib1g-dev libssl-doc libssl-dev libsqlite3-dev libyaml-dev
   libxml2-dev libmysqlclient-dev ruby2.0-dev libxslt1-dev ruby-switch
 }.each do |name|
   package name do
